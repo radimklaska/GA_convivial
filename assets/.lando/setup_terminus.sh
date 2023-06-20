80fe0b9f9aafc679dafd38c2359e5733226344b9
Radim Klaska <radim@morpht.com>
2023-06-20 23:01:26 +0200

    GA profile

--- assets/.lando/setup_terminus.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd /usr/local/bin/
-curl -L https://github.com/pantheon-systems/terminus/releases/download/$(curl --silent "https://api.github.com/repos/pantheon-systems/terminus/releases/latest" | perl -nle'print $& while m{"tag_name": "\K.*?(?=")}g')/terminus.phar --output terminus
-chmod +x terminus
-chown www-data:www-data terminus
